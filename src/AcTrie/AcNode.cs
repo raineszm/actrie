b9b5b93b1e924f01c2baea5d198ee3caf18c7a24
Zach Raines <raineszm@gmail.com>
2021-01-29 16:00:02 -0500

    write ACNode class

---
+++ src/AcTrie/AcNode.cs
@@ -0,0 +1,240 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AcTrie
+{
+    public class AcNode<TToken, TValue> : IDictionary<IEnumerable<TToken>, TValue>
+        where TValue : struct
+        where TToken : notnull
+    {
+        private readonly IDictionary<TToken, AcNode<TToken, TValue>>
+            _children = new Dictionary<TToken, AcNode<TToken, TValue>>();
+
+        public TValue? Value;
+        public SuffixEdge? Suffix;
+        public bool IsReadOnly => false;
+
+        public bool TryGetValue(IEnumerable<TToken> key, out TValue value)
+        {
+            value = default;
+            var keyList = key.ToList();
+            if (!keyList.Any()) return false;
+            var node = IterPath(keyList.ToList()).Last();
+            if (node == this || node?.Value is null) return false;
+            value = (TValue) node.Value;
+            return true;
+        }
+
+        public TValue this[IEnumerable<TToken> key]
+        {
+            get
+            {
+                if (!TryGetValue(key, out var value))
+                {
+                    throw new KeyNotFoundException(key.ToString());
+                }
+
+                return value;
+            }
+
+            set => Add(key, value);
+        }
+
+        public void Add(IEnumerable<TToken> key, TValue value)
+        {
+            var keyList = key.ToList();
+
+            // Key is exhausted
+            // We are setting this node
+            if (!keyList.Any())
+            {
+                Value = value;
+                return;
+            }
+
+            var child = _children[keyList.First()];
+            // If no matching child, create it
+            if (child is null)
+            {
+                child = new AcNode<TToken, TValue>();
+                _children[keyList.First()] = child;
+            }
+
+            child[keyList.Skip(1)] = value;
+        }
+
+        public void Add(KeyValuePair<IEnumerable<TToken>, TValue> item)
+        {
+            Add(item.Key, item.Value);
+        }
+
+        private bool RemoveImpl(IEnumerable<TToken> key, TValue? value = null)
+        {
+            var keyList = key.ToList();
+
+            if (!keyList.Any()) return false;
+
+            var nodes = IterPath(keyList).ToList();
+            if (nodes.Count < keyList.Count + 1) return false;
+
+            var target = nodes[^1];
+            nodes.RemoveAt(nodes.Count - 1);
+            if (target?.Value is null) return false;
+
+
+            if (value is not null && !value.Equals(target.Value)) return false;
+
+            TrimNodes(keyList, nodes);
+
+            return true;
+        }
+
+        private static void TrimNodes(IReadOnlyList<TToken> keyList, IReadOnlyList<AcNode<TToken, TValue>?> nodes)
+        {
+            for (var i = Math.Min(keyList.Count, nodes.Count) - 1; i >= 0; i--)
+            {
+                var parent = nodes[i];
+                if (parent is null)
+                {
+                    throw new NullReferenceException("accessed a child of an undefined node");
+                }
+
+                var edge = keyList[i];
+                parent._children.Remove(edge);
+                if (parent._children.Count > 0 || parent.Value is not null)
+                {
+                    break;
+                }
+            }
+        }
+
+        public bool Remove(IEnumerable<TToken> key)
+        {
+            return RemoveImpl(key);
+        }
+
+        public bool Remove(KeyValuePair<IEnumerable<TToken>, TValue> item)
+        {
+            return RemoveImpl(item.Key, item.Value);
+        }
+
+
+        public bool ContainsKey(IEnumerable<TToken> key)
+        {
+            return TryGetValue(key, out var _);
+        }
+
+        public bool Contains(KeyValuePair<IEnumerable<TToken>, TValue> item)
+        {
+            return TryGetValue(item.Key, out var value) && item.Value.Equals(value);
+        }
+
+
+        public int Count
+        {
+            get
+            {
+                var total = 0;
+                if (Value is not null)
+                {
+                    total++;
+                }
+
+                return total + _children.Values.Sum(child => child.Count);
+            }
+        }
+
+
+        public void Clear()
+        {
+            Value = null;
+            _children.Clear();
+        }
+
+        public IEnumerable<AcNode<TToken, TValue>> Nodes()
+        {
+            yield return this;
+            foreach (var child in _children.Values)
+            {
+                foreach (var node in child.Nodes())
+                {
+                    yield return node;
+                }
+            }
+        }
+
+        public IEnumerator<KeyValuePair<IEnumerable<TToken>, TValue>> GetEnumerator()
+        {
+            if (Value is not null)
+            {
+                yield return new KeyValuePair<IEnumerable<TToken>, TValue>(Array.Empty<TToken>(), (TValue) Value);
+            }
+
+            foreach (var (c, child) in _children)
+            {
+                foreach (var (k, v) in child)
+                {
+                    yield return new KeyValuePair<IEnumerable<TToken>, TValue>(
+                        new[] {c}.Concat(k), v
+                    );
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void CopyTo(KeyValuePair<IEnumerable<TToken>, TValue>[] array, int arrayIndex)
+        {
+            var i = arrayIndex;
+            foreach (var item in this)
+            {
+                array[i++] = item;
+            }
+        }
+
+        public ICollection<IEnumerable<TToken>> Keys => this.Select(kv => kv.Key).ToList();
+        public ICollection<TValue> Values => this.Select(kv => kv.Value).ToList();
+
+        private IEnumerable<AcNode<TToken, TValue>?> IterPath(
+            IReadOnlyList<TToken> key
+        )
+        {
+            yield return this;
+            if (!key.Any())
+            {
+                yield break;
+            }
+
+            var child = _children[key[0]];
+            if (child is not null)
+            {
+                foreach (var node in child.IterPath(key.Skip(1).ToList()))
+                {
+                    yield return node;
+                }
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+
+        public class Needle
+        {
+            public int Start;
+            public int End;
+            public TValue Value;
+        }
+
+        public class SuffixEdge
+        {
+            public AcNode<TToken, TValue> Node;
+            public int Shift;
+        }
+    }
+}
--- test/AcTrie.Test/AcTrieTest.cs
+++ test/AcTrie.Test/AcTrieTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Xunit;
 using CITrie = AcTrie.AcTrie<char, int>;
+using CINeedle = AcTrie.AcNode<char, int>.Needle;
 
 namespace AcTrie.Test
 {
@@ -20,12 +21,12 @@ namespace AcTrie.Test
 
         public static IEnumerable<object[]> SearchData()
         {
-            yield return new object[] {"ar", Array.Empty<CITrie.Needle>()};
+            yield return new object[] {"ar", Array.Empty<CINeedle>()};
             yield return new object[]
             {
                 "art", new[]
                 {
-                    new CITrie.Needle
+                    new CINeedle
                     {
                         Start = 0,
                         End = 3,
@@ -34,14 +35,14 @@ namespace AcTrie.Test
                 }
             };
 
-            yield return new object[] {"artsy", new[] {new CITrie.Needle {Start = 0, End = 3, Value = 1}},};
+            yield return new object[] {"artsy", new[] {new CINeedle {Start = 0, End = 3, Value = 1}},};
             yield return new object[]
             {
                 "soars",
                 new[]
                 {
-                    new CITrie.Needle {Start = 0, End = 4, Value = 3},
-                    new CITrie.Needle {Start = 1, End = 5, Value = 2},
+                    new CINeedle {Start = 0, End = 4, Value = 3},
+                    new CINeedle {Start = 1, End = 5, Value = 2},
                 }
             };
             yield return new object[]
@@ -49,15 +50,15 @@ namespace AcTrie.Test
                 "artsoar",
                 new[]
                 {
-                    new CITrie.Needle {Start = 0, End = 3, Value = 1},
-                    new CITrie.Needle {Start = 3, End = 7, Value = 3},
+                    new CINeedle {Start = 0, End = 3, Value = 1},
+                    new CINeedle {Start = 3, End = 7, Value = 3},
                 }
             };
         }
 
         [Theory]
         [MemberData(nameof(SearchData))]
-        public void Search_GivenAString_ReturnsAllMatchingSubstrings(string str, AcTrie<char, int>.Needle[] matches)
+        public void Search_GivenAString_ReturnsAllMatchingSubstrings(string str, CINeedle[] matches)
         {
             SoarAcTrie.Search(str).Should().BeEquivalentTo(matches);
         }
@@ -65,15 +66,15 @@ namespace AcTrie.Test
         public static IEnumerable<object[]> LongestMatchData()
         {
             yield return new object[] {"ar", null};
-            yield return new object[] {"art", new CITrie.Needle {Start = 0, End = 3, Value = 1}};
-            yield return new object[] {"artsy", new CITrie.Needle {Start = 0, End = 3, Value = 1}};
-            yield return new object[] {"soars", new CITrie.Needle {Start = 0, End = 4, Value = 3}};
-            yield return new object[] {"artsoar", new CITrie.Needle {Start = 3, End = 7, Value = 3}};
+            yield return new object[] {"art", new CINeedle {Start = 0, End = 3, Value = 1}};
+            yield return new object[] {"artsy", new CINeedle {Start = 0, End = 3, Value = 1}};
+            yield return new object[] {"soars", new CINeedle {Start = 0, End = 4, Value = 3}};
+            yield return new object[] {"artsoar", new CINeedle {Start = 3, End = 7, Value = 3}};
         }
 
         [Theory]
         [MemberData(nameof(LongestMatchData))]
-        public void LongestMatch_GivenString_ReturnsLongestMatch(string str, CITrie.Needle match)
+        public void LongestMatch_GivenString_ReturnsLongestMatch(string str, CINeedle match)
         {
             SoarAcTrie.LongestMatch(str).Should().Be(match);
         }
@@ -84,7 +85,7 @@ namespace AcTrie.Test
             var trie = new AcTrie<char, int>();
             trie.Add("point", 1);
             trie.Add("conduct", 2);
-            trie.LongestMatch("pconduct").Should().Be(new AcTrie<char, int>.Needle
+            trie.LongestMatch("pconduct").Should().Be(new CINeedle
             {
                 Start = 1,
                 End = 8,
